666177c1c5e8793a9902ba062f6eda2ad6a7f0aa
Ricardo Cristino <ricardocristino@gmail.com>
2017-10-26 01:19:51 +0200

    Add ip recognition in gui

--- gui/views.py
+++ gui/views.py
@@ -6,22 +6,36 @@ import json
 @gui.route('/')
 @gui.route('/index')
 def index():
+    addr = {'ip': get_ip(), 'port': 5000}
     title = {'title': 'Auto-Garden'}
     cdate = {'cdate': time.strftime("%c")} 
     current = {'pump': False, 'moisture': 567}
    
     return render_template("index.html",
+                           addr=addr,
                            title=title,
                            cdate=cdate,
                            current=current)
 
 @gui.route("/pump")
 def button_click():
+    addr = {'ip': get_ip(), 'port': 5000}
     title = {'title': 'Auto Garden'}
     cdate = {'cdate': time.strftime("%c")}
     current = {'pump': True, 'moisture': 567}
    
     return render_template("index.html",
+                           addr=addr,
                            title=title,
                            cdate=cdate,
                            current=current)
+
+def get_ip():
+    import socket
+    import os
+    
+    gw = os.popen("ip -4 route show default").read().split()
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect((gw[2], 0))
+    ipaddr = s.getsockname()[0]
+    return ipaddr
