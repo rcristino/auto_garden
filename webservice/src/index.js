eb97cbf6acf97559f47824792a3cc83c5a150f14
David Rueda <david@amerbank.com>
2018-04-18 00:40:38 +0200

    mongodb post logs

--- webservice/src/index.js
+++ webservice/src/index.js
@@ -1,12 +1,34 @@
-// Require the framework and instantiate it
 const fastify = require('fastify')()
 
-// Declare a route
+fastify.register(require('fastify-mongodb'), {
+  url: 'mongodb://localhost:27017/auto_garden_webapp'
+})
+
+const MONGO_CONNEXION_ISSUE = 0
+
+function failFast(msg, code) {
+  console.error(msg)
+  process.exit(code)
+}
+
 fastify.get('/', function (request, reply) {
   reply.send({ hello: 'world' })
 })
 
-// Run the server!
+fastify.post('/log', function (request, reply) {
+  console.log(request.payload)
+
+  const db = this.mongo.db
+  db.collection('logs', (err, col) => {
+    if (err) return reply.send(err)
+
+    col.insert(request.payload.data, (err, result) => {
+      reply.send(result)
+    })
+  })
+})
+
+// RUN SERVER
 fastify.listen(process.env.WEBSVC_PORT || 3000, '0.0.0.0', function (err) {
   if (err) throw err
   console.log(`server listening on ${fastify.server.address().port}`)
