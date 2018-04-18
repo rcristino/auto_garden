66122fcb332b781851b6323d3e0b5b2c3c713dd7
David Rueda <david@amerbank.com>
2018-04-18 14:52:02 +0200

    console log request body

--- webservice/src/index.js
+++ webservice/src/index.js
@@ -24,6 +24,7 @@ fastify.get('/', function (request, reply) {
 
 fastify.post('/log', function (request, reply) {
 
+  pino.info(request.body)
   const payload = JSON.parse(request.body)
 
   pino.info('Going to store log.')
